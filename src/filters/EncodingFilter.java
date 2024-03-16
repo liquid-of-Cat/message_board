6bbc0cb65033253a14d8b69c4e049bc9b8fb772d
LAstc <LAstc@LASTCAT>
2024-03-17 00:50:08 +0900

    NewSevlet.javaの作成

--- src/filters/EncodingFilter.java
+++ src/filters/EncodingFilter.java
@@ -1,6 +1,7 @@
 package filters;
 
 import java.io.IOException;
+
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
 import javax.servlet.FilterConfig;
@@ -19,21 +20,18 @@ public class EncodingFilter implements Filter {
      * Default constructor. 
      */
     public EncodingFilter() {
-        // TODO Auto-generated constructor stub
     }
 
 	/**
 	 * @see Filter#destroy()
 	 */
 	public void destroy() {
-		// TODO Auto-generated method stub
 	}
 
 	/**
 	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
 	 */
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-		// TODO Auto-generated method stub
 		// place your code here
 
 		// pass the request along the filter chain
@@ -44,7 +42,7 @@ public class EncodingFilter implements Filter {
 	 * @see Filter#init(FilterConfig)
 	 */
 	public void init(FilterConfig fConfig) throws ServletException {
-		// TODO Auto-generated method stub
+
 	}
 
 }
